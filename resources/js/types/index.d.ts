3c156006fd5c514ee1d4a4b8134f9deabaf0f929
yousef tarek <youssefjoo1110@gmail.com>
2025-08-12 12:56:03 +0300

    intial commit

---
+++ resources/js/types/index.d.ts
@@ -0,0 +1,62 @@
+import { Config } from 'ziggy-js';
+import { PageProps } from '@inertiajs/react';
+import { type LucideProps } from 'lucide-react';
+import { ReactNode, ComponentType } from 'react';
+
+
+// User type
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  avatar?: string;  
+  email_verified_at:string,
+}
+
+// Breadcrumb item type
+export interface BreadcrumbItem {
+  title: string;
+  href: string;
+  icon?: ComponentType<LucideProps> | ReactNode | string | ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>;
+}
+
+// Navigation item type
+export interface NavItem {
+  name?: string;
+  href: string;
+  title:string,
+  icon: React.ReactNode|string;
+  current?: boolean;
+}
+
+
+
+
+export type PageProps<
+    T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+    auth: {
+        user: User;
+    };
+    ziggy: Config & { location: string };
+};
+
+
+// Shared data type (passed from Laravel backend)
+export interface SharedData extends PageProps {
+  user: User;
+  name:ReactNode;
+  quote :{
+    author:string,
+    message:string,
+  };
+  flash?: {
+    success?: string;
+    error?: string;
+  };
+  // Add other shared properties as needed
+}
+
+
+
+export type IconType = ComponentType<LucideProps> | ReactNode;
